d444976d7b9fdcc306c9937b51f7622d0c1ed3fd
jcExtra <161068145+jcExtra@users.noreply.github.com>
2024-03-22 00:42:25 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-86ffdf95-46d8-4a6e-be21-aea689bb298a/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}'https://eth1.lava.build/lava-referer-86ffdf95-46d8-4a6e-be21-aea689bb298a/)
 
 
 echo "$curl_output" >> Update.tsx
